2a3568e430f023c31d13037a2677fb5978c2040d
Siddharth Kumar <siddharth.kumar@utexas.edu>
2020-07-23 19:39:13 -0500

    some more bones

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const port = 3000
 
 
 
-var mydir = path.resolve(__dirname)
+var mydir = path.resolve(__dirname) + '/public'
+
+app.use('/favicon.ico', express.static('favicon.ico'));
+
+app.use(express.static(mydir));
 
 app.get('/', (req, res) => res.sendFile(path.join(mydir, 'index.html')))
+
+// Left as an example
+// app.get('/users', (req, res) => res.sendFile(path.join(mydir, 'users.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
