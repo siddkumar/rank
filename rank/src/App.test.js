de7ab6323fb38482397efb532e9b595fede86b3e
Siddharth Kumar <siddharth.kumar@utexas.edu>
2022-07-21 23:23:01 -0700

    Draggable Item Tutorial

--- rank/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  var x = 1;
-  expect(x).toBe(1);
-});
--- rank/src/components/blobs/blobManager.tsx
+++ rank/src/components/blobs/blobManager.tsx
@@ -1,19 +1,62 @@
-import React from 'react';
-import Blob from '../blobs/Blob';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
+import Blob from '../../models/Blob';
 
 export interface BlobManagerProps {
     blobs: Blob[]
 }
 
 function BlobManager (props : BlobManagerProps) {
+
+    const defaultList = props.blobs.map((blob) => { return blob.blobName; } )
+    const [blobList, setBloblist] = useState(defaultList);
+
     return (
         <div>
-            <div className="App-demo">blobs go here {props.blobs.length}</div> 
-            <ul>
-                {getBlobs(props.blobs)}
-            </ul>
+            <DragDropContext onDragEnd={handleDrop}>
+                <Droppable droppableId="list-container">
+          {(provided) => (
+
+
+            <div
+              className="list-container"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {blobList.map((item, index) => (
+                <Draggable key={item} draggableId={item} index={index}>
+                  {(provided) => (
+                    <div
+                      className="item-container"
+                      ref={provided.innerRef}
+                      {...provided.dragHandleProps}
+                      {...provided.draggableProps}
+                    >
+                      {item}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+
+            </DragDropContext>
+            {/* <ul>
+                {getBlobs(blobList)}
+            </ul> */}
         </div>
     );
+
+    function handleDrop(droppedItem: DropResult) {
+        var updatedList = [...blobList]
+        const [reorderedItem] = updatedList.splice(droppedItem.source.index, 1)
+        // Add dropped item
+        updatedList.splice(droppedItem!.destination!.index, 0, reorderedItem);
+        // Update State
+        setBloblist(updatedList);
+    }
 
 
     function getBlobs(blobs: Blob[]){
