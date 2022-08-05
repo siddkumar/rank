2fe367a639858ee6232c1074765ab5572e05f1fa
Siddharth Kumar <siddharth0922@gmail.com>
2022-08-04 23:51:35 -0500

    RANK-6: Header infra

--- rank/src/pages/header.tsx
+++ rank/src/pages/header.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import logo from "../resources/logo.png"
 
 export interface HeaderProps {
 }
 
 function Header(props: HeaderProps) {
   return (
-    <div className="App-header">
-      <p>rank anything</p>
+    <div className="app-header">
+      <div className="header-left">
+        <img src={logo} alt="logo" className="header-logo"/>
+        <p className="header-title">rank anything</p>
+      </div>
+      <div className="header-right">
+        <a href="/home">home</a>
+        <a href="/create">create</a>
+        <a href="/demo">demo</a>
+        <a href="/templates">templates</a>
+        <a href="/mystuff">ranks</a>
+      </div>
     </div>
   );
 }
