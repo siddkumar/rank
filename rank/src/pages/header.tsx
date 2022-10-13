7cee8fa0514812bb5cfaedc5aa5df00a2b443a51
Siddharth Kumar <siddharth0922@gmail.com>
2022-10-13 17:52:41 -0500

    Revert "RANK-14: Auth0 implementation"
    This reverts commit 87eb97cc33974a959c9c0c02e644ef2e99f67c22.

--- rank/src/pages/header.tsx
+++ rank/src/pages/header.tsx
@@ -1,39 +1,9 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import logo from "../resources/logo.png";
 
 export interface HeaderProps {}
 
 function Header(props: HeaderProps) {
-  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
-
-  function renderAuthSection() {
-    if (isAuthenticated) {
-      return (
-        <>
-          <div>Hi {user?.given_name}! </div>
-          <button
-            className="login-button-styles"
-            onClick={() => logout({ returnTo: window.location.origin })}
-          >
-            Log Out
-          </button>
-        </>
-      );
-    }
-    return (
-      <button
-        onClick={() => {
-          console.log(window.location.origin);
-          loginWithRedirect();
-        }}
-        className="login-button-styles"
-      >
-        Log In
-      </button>
-    );
-  }
-
   return (
     <div className="app-header">
       <div className="header-left">
@@ -46,7 +16,6 @@ function Header(props: HeaderProps) {
         <a href="/demo">demo</a>
         <a href="/templates">templates</a>
         <a href="/mystuff">ranks</a>
-        <>{renderAuthSection()}</>
       </div>
     </div>
   );
