19b15e8e83a20cde658dc63add9c3ef7f0ebc5c0
sidd <siddharth0922@gmail.com>
2023-05-31 16:20:59 -0500

    fix sign in issue

--- rank/src/index.tsx
+++ rank/src/index.tsx
@@ -4,12 +4,21 @@ import "./styles/index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import AuthSetUp from "./components/auth/authSetUp";
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+}
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthSetUp />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
