719e5bc6212616f49577c838a1644d27024fade8
sidd <siddharth0922@gmail.com>
2026-02-15 23:34:04 -0600

    more deploy changes

--- rank/src/react-app-env.d.ts
+++ rank/src/react-app-env.d.ts
@@ -1,1 +1,46 @@
 /// <reference types="react-scripts" />
+
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.webp" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.sass" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
